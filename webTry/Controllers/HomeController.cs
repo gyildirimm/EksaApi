43f1eadb58156c0f077784576b24c08c8d567648
gyildirimm <goktugyildirim22@gmail.com>
2020-04-25 14:57:29 +0300

    Update HomeController.cs

--- webTry/Controllers/HomeController.cs
+++ webTry/Controllers/HomeController.cs
@@ -42,7 +42,7 @@ namespace webTry.Controllers
                 }  
             }else
             {
-                token = "başarısız";
+                token = "";
             }
             return token;
         }
